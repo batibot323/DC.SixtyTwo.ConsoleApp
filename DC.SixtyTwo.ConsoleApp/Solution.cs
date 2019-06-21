7d588babfab9c70c5dcb0c3954ca88c9002fa02a
Hani Herbert Ho <hani.ho@pointwest.com.ph>
2019-06-21 18:19:25 +0800

    4 - Green 3 - Red

--- DC.SixtyTwo.ConsoleApp/Solution.cs
+++ DC.SixtyTwo.ConsoleApp/Solution.cs
@@ -6,11 +6,65 @@ namespace DC.SixtyTwo.ConsoleApp
 {
     public class Solution
     {
+        private int maxX, maxY, currentX, currentY, answer;
+
+        public Solution()
+        {
+            maxX = 0;
+            maxY = 0;
+            answer = 0;
+            currentX = 0;
+            currentY = 0;
+        }
         public int Solve (int m, int n)
         {
-            int distanceFromX = m - 1;
-            int distanceFromY = n - 1;
-            return distanceFromX + distanceFromY;
+            maxX = m - 1;
+            maxY = n - 1;
+            TakeAStep();
+            return answer;
+        }
+
+        private void TakeAStep()
+        {
+            if (IsFinished())
+            {
+                answer++;
+            }
+            else
+            {
+                if (currentX < maxX)
+                {
+                    StepAlongX();
+                }
+                if (currentY < maxY)
+                {
+                    StepAlongY();
+                }
+            }
+        }
+
+        private void StepAlongY()
+        {
+            currentY++;
+            TakeAStep();
+        }
+
+        private void StepAlongX()
+        {
+            currentX++;
+            TakeAStep();
+        }
+
+        private bool IsFinished()
+        {
+            if (currentX == maxX && currentY == maxY)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
